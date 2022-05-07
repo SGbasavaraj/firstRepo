ccfed219563e98abedd1ea4ffaa29c51f30e5539
basus <basus@LAPTOP-1ELLEUV1>
2022-05-07 20:25:24 +0530

    second commit

---
+++ testGit/src/testGit/gitTestClass.java
@@ -0,0 +1,20 @@
+package testGit;
+
+public class gitTestClass {
+
+	public static void main(String[] args) {
+
+
+		int a=1,b=1,c=a+b;
+		System.out.println(c);
+		
+		System.out.println("second commit");
+		System.out.print("second commit");
+
+		
+	}
+
+
+
+	}
+
